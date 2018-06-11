72497a6d838f1e4f124f6e91ba780ac9814e39b8
harichintha7 <37565219+harichintha7@users.noreply.github.com>
2018-06-11 13:24:44 -0500

    Initial

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FuzzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FuzzBuzzClass fzclass = new FuzzBuzzClass();
+            fzclass.PrintOut(100);
+            Console.ReadLine();
+        }
+    }
+
+    class FuzzBuzzClass
+    {
+       public void PrintOut(int N)
+        {
+            for(int i = 1; i <= N; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
